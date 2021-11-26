a0b8425776a20612b49608f6d6077f28e3eaa006
Md. Jubayer Hossain Abir <41603187+Jubayer-Hossain-Abir-404@users.noreply.github.com>
2021-11-26 23:36:40 +0600

    Update

---
+++ src/components/Header.js
@@ -0,0 +1,33 @@
+//import React from 'react'
+import PropTypes from 'prop-types'
+import Button from './Button'
+
+const Header = ({ title }) => {
+    const onClick = () =>{
+        console.log('Click')
+    }
+    return (
+        <header className='header'>
+            <h1 >{title}</h1>
+            <Button color='green' text='Add' onClick ={onClick}/>
+            {/* <Button color='blue' text='Hello1'/>
+            <Button color='red' text='Hello2'/> */}
+        </header>
+    )
+}
+
+Header.defaultProps = {
+    title: 'Task Tracker',
+}
+
+Header.propTypes = {
+    title: PropTypes.string.isRequired,
+}
+
+
+//css in js
+// const headingStyle = {
+//     color: 'red', 
+//     backgroundColor: 'black'
+// }
+export default Header
