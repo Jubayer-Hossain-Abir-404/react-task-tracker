7567a0275b7c707df201ca7699022906dcd1c320
Md. Jubayer Hossain Abir <41603187+Jubayer-Hossain-Abir-404@users.noreply.github.com>
2021-11-28 07:10:17 +0600

    Update

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -24,10 +24,10 @@ const Tasks = ({ tasks, onDelete, onToggle}) => {
     
     return (
         <>
-           {tasks.map((task) => (
+           {tasks.map((task, index) => (
             //<h3 key={task.id}>{task.text}</h3>
             <Task 
-                key={task.id} 
+                key={index} 
                 task={task} 
                 onDelete={onDelete} 
                 onToggle={onToggle}
